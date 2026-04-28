e4493df7d0e16a677a77779be320213fa29fe9f4
Heath Shepard <heath.shepard@kw.com>
2026-04-28 11:29:09 -0500

    Deploy workspace-B6piasuy

--- api/speak.js
+++ api/speak.js
@@ -58,7 +58,17 @@ export default async function handler(req, res) {
       return res.status(500).json({ ok: false, error: 'Server configuration error' });
     }
 
-    const preprocessText = (t) => t
+    const formatSpokenDate = (s) => {
+      const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+      const ordinals = ['','1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th','13th','14th','15th','16th','17th','18th','19th','20th','21st','22nd','23rd','24th','25th','26th','27th','28th','29th','30th','31st'];
+      return s.replace(/\b(\d{4})-(\d{2})-(\d{2})\b/g, (match, year, month, day) => {
+        const m = months[parseInt(month, 10) - 1];
+        const d = ordinals[parseInt(day, 10)];
+        if (!m || !d) return match;
+        return `${m} ${d}, ${year}`;
+      });
+    };
+    const preprocessText = (t) => formatSpokenDate(t)
       .replace(/\bTX\b/g, 'Texas')
       .replace(/\bBoerne\b/gi, 'Bernie')
       .replace(/\bBexar\b/gi, 'Bear')
